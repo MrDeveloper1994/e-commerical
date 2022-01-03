a9dfdf4458efd298859b372fa20ff34a6925b4c2
MasterDeveloper <sina.master1994@gmail.com>
2022-01-03 13:05:00 +0330

    front-end section3

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddCors();
 var app = builder.Build();
 using var scope = app.Services.CreateScope();
 var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
@@ -37,7 +37,7 @@ if (app.Environment.IsDevelopment())
 }
 
 // app.UseHttpsRedirection();
-
+app.UseCors(opt => { opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000"); });
 app.UseAuthorization();
 
 app.MapControllers();
